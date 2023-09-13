1599bcf313177e1ba49ef6be8917fd9f11b2195e
MahmoudKhalid <mahmoud0122549@gmail.com>
2023-09-13 18:27:15 +0300

    update localStorage

--- client/src/components/UpdateForm.js
+++ client/src/components/UpdateForm.js
@@ -11,13 +11,6 @@ function UpdateForm({ userId, userToken }) {
   const updateUser = async (e) => {
     e.preventDefault();
     try {
-      console.log(
-        JSON.stringify({
-          name: userDataUpdate.name,
-          email: userDataUpdate.email,
-          password: userDataUpdate.password,
-        })
-      );
       const response = await fetch(`http://localhost:5000/users/${userId}`, {
         method: "PATCH",
         body: JSON.stringify({
@@ -47,26 +40,31 @@ function UpdateForm({ userId, userToken }) {
       }
       // console.log(await response.json());
       const updatedData = await response.json();
-      console.log(updatedData);
+      console.log(updatedData[0]);
       setUserDataUpdate({
         ...userDataUpdate,
         name: "",
         email: "",
         password: "",
       });
-      // window.location.reload();
-      const json = JSON.parse(localStorage.getItem("data"));
-      json.user.name = updatedData.name;
-      json.user.email = updatedData.email;
-      json.user.password = updatedData.password;
-      
-      // console.log(localStorage.getItem("data").user);
+      const existingData = localStorage.getItem("data");
+      const existingDataParsed = JSON.parse(existingData);
+
+      console.log(existingDataParsed);
+
+      existingDataParsed.user.name = updatedData[0].name;
+      existingDataParsed.user.email = updatedData[0].email;
+      existingDataParsed.user.password = updatedData[0].password;
+
+      const updatedDataStr = JSON.stringify(existingDataParsed);
+      console.log(updatedDataStr);
+      localStorage.setItem("data", updatedDataStr);
+      window.location.reload();
     } catch (e) {
       console.log(e);
       throw new Error(e);
     }
   };
-  console.log(JSON.parse(localStorage.getItem("data")).user);
 
   return (
     <form className={styles.container} onSubmit={updateUser}>
