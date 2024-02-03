623e0184ba242c3aeab20a0481dc68349fbb709f
Mahmoud Khalid <103892504+MahmoudKhalid22@users.noreply.github.com>
2024-02-02 18:07:54 -0800

    chore: add custom error page

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
+const path = require("path");
 const mongoose = require("./config/dbConnection");
 const userRouter = require("./router/user");
 const tableRouter = require("./router/tables");
@@ -8,12 +9,18 @@ const { docs } = require("./utils/swagger");
 
 const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.use(express.json());
 app.use(cors());
 app.use("/user", userRouter);
 app.use(tableRouter);
 docs(app);
 
+app.get("*", (req, res) => {
+  res.render("404");
+});
 const port = process.env.PORT;
 
 app.listen(port, () => console.log("Server is running on port " + port));
